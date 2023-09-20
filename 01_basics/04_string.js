1dc75ab566c7a93f62c9a52fb854833970a2b25c
Yogesh22innobit <145459804+Yogesh22innobit@users.noreply.github.com>
2023-09-20 12:12:32 +0000

    added array , string and dates

---
+++ 01_basics/04_string.js
@@ -0,0 +1,4 @@
+let text1 = "this is just a \"testing\""
+console.log(text1);
+let text2 = "this is just a testing\""
+console.log(text2);
---
+++ 01_basics/05_Arrays.js
@@ -0,0 +1,16 @@
+let Vehicals = ["car", "cycle", "bus", "truck"]
+//console.log(Vehicals[2]) 
+//console.log(Vehicals[0]) 
+//console.log(Vehicals.length) 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//fruits.pop();
+//console.log(fruits);
+//console.table(fruits)
+/*let length = fruits.push("kiwi");
+console.log(length)*/
+fruits.unshift("papaya")
+//console.log(fruits);
+//console.table(fruits)
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a - b});
+console.log(points)
