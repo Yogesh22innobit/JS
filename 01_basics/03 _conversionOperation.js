5d72150e9969d7d2af2f3a22edb19e4f1d408e3b
Yogesh22innobit <145459804+Yogesh22innobit@users.noreply.github.com>
2023-09-20 10:15:40 +0000

    add convers

---
+++ 01_basics/03 _conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isloggedin = ""
+let booleanIsLoggedIn = Boolean(isloggedin)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true
+// "" => false 
+// "string" => true
+let someNumber = 32
+let StringNumber = String(someNumber)
+console.log(StringNumber)
+console.log( typeof StringNumber)
+
+
